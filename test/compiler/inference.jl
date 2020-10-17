22368c924eef296850f8166e1f29ebbfda643f45
Keno Fischer <keno@juliacomputing.com>
2020-10-17 17:56:06 -0400

    Add a macro to opt into aggressive constprop
    Right now aggressive constprop is essentially tied to the inlining
    threshold (or to their name being `getproperty` or `setproperty!`
    respectively, which can be both somewhat brittle if the inlining cost
    changes and insufficient when you do really know that const prop
    would be beneficial even if the function is not inlineable. This
    adds a simple macro that can be used to manually annotate methods
    to force aggressive constprop on them.

--- test/compiler/inference.jl
+++ test/compiler/inference.jl
@@ -3008,3 +3008,15 @@ f38888() = S38888(Base.inferencebarrier(3))
 @test f38888() isa S38888
 g38888() = S38888(Base.inferencebarrier(3), nothing)
 @test g38888() isa S38888
+
+# @aggressive_constprop
+@noinline g_nonaggressive(y, x) = Val{x}()
+@noinline @Base.aggressive_constprop g_aggressive(y, x) = Val{x}()
+
+f_nonaggressive(x) = g_nonaggressive(x, 1)
+f_aggressive(x) = g_aggressive(x, 1)
+
+# The first test just makes sure that improvements to the compiler don't
+# render the annotation effectless.
+@test Base.return_types(f_nonaggressive, Tuple{Int})[1] == Val
+@test Base.return_types(f_aggressive, Tuple{Int})[1] == Val{1}
